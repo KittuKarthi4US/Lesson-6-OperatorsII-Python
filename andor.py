0caa002ee03bce2da340ad5b31fe235b49933abc
KittuKarthi4US <krishna.nucherla@gmail.com>
2026-06-22 20:12:23 +0100

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,23 @@
+x = 10
+y = 5
+z = -10
+
+if x and y and z:
+    print("Good")
+else:
+    print('Bad')
+
+
+a = 34
+b = -2
+
+if a > 0 or b > 0:
+    print('One or all is biger than 0')
+else:
+    print('None are bigger than 0')
+
+
+if a > 0 and b > 0:
+    print('Both are bigger than 0')
+else:
+    print('None are bigger than 0')
